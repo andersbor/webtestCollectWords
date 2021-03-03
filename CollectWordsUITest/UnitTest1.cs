b7aca52abf12ee7ef62434dc9b78bfe4dcc8902f
Anders Børjesson <anbo@zealand.dk>
2021-03-03 08:22:51 +0100

    test runs

--- CollectWordsUITest/UnitTest1.cs
+++ CollectWordsUITest/UnitTest1.cs
@@ -1,6 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Support.UI;
 
@@ -9,14 +10,14 @@ namespace CollectWordsUITest
     [TestClass]
     public class UnitTest1
     {
-        private static readonly string DriverDirectory = "C:\\seleniumDrivers2";
+        private static readonly string DriverDirectory = "C:\\webDrivers";
         private static IWebDriver _driver;
 
         [ClassInitialize]
         public static void Setup(TestContext context)
         {
-            // _driver = new ChromeDriver(DriverDirectory); // fast
-             _driver = new FirefoxDriver(DriverDirectory);  // slow
+             _driver = new ChromeDriver(DriverDirectory); // fast
+            // _driver = new FirefoxDriver(DriverDirectory);  // slow
             // _driver = new EdgeDriver(DriverDirectory); // times out ... not working
         }
 
@@ -29,7 +30,8 @@ namespace CollectWordsUITest
         [TestMethod]
         public void TestMethod1()
         {
-            _driver.Navigate().GoToUrl("http://localhost:3000/");
+            //_driver.Navigate().GoToUrl("http://localhost:3000/");
+            _driver.Navigate().GoToUrl("file:///C:/andersb/javascript/collectwords/index.htm");
             Assert.AreEqual("Collect words", _driver.Title);
 
             IWebElement inputElement = _driver.FindElement(By.Id("wordInput"));
@@ -60,7 +62,6 @@ namespace CollectWordsUITest
 
             showButton.Click();
             Assert.AreEqual("empty", outputElement.Text);
-
         }
     }
 }
